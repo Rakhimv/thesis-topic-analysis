af592de2aecd026f9e3430bc84e9025ac734c556
rakhimv <azizbekrakhimv@gmail.com>
2026-05-13 20:11:24 +0300

    plotly init

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import plotly.express as px
+
 
 data = pd.read_excel("thesis_data.xlsx")
-print(sorted(data["Graduation"]))
+yrs = sorted(data["Graduation"])
 
+yrs_count = data["Graduation"].value_counts().sort_index().reset_index()
+yrs_count.columns = ["year", "count"]
+
+print(yrs_count)
+
+fig = px.bar(yrs_count, x="year", y="count", title="Col-vo rabot")
+fig.show()
