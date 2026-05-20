a097c7334cde5f9ff4968bc4640f7a8c0663e6aa
ArtyomKidinov <130593776+ArtyomKidinov@users.noreply.github.com>
2026-05-21 01:37:26 +0300

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -1,6 +1,10 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+from categories import apply_categories
+import pandas as pd 
+import plotly.express as px
 
 data = pd.read_excel("thesis_data.xlsx")
-print(sorted(data["Graduation"]))
+data = apply_categories(data)
+pivot = pd.crosstab(data["Advisor"], data["category"])
 
+fig = px.imshow(pivot, title="Связь научных руководителей и тематик", aspect="auto", color_continuous_scale="Blues")
+fig.show()
