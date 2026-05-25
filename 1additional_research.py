992306a2502f462c14eb9d24dde406bff34a324e
User Konstantin <koskakoska15@mail.ru>
2026-05-25 17:16:47 +0300

    Fix a mistake

--- 1additional_research.py
+++ 1additional_research.py
@@ -9,7 +9,7 @@ data['Advisor'] = data['Advisor'].astype(str).str.strip() #берём колон
 data['Advisor_mapped'] = data['Advisor'].map(teacher_eng_mapping).fillna(data['Advisor'])
 data['Advisor_mapped'] = data['Advisor_mapped'].replace(['nan', 'None', ''], pd.NA) # Колонка, где пустые заменены на флаг NA 
 
-advisor_counts = data['Advisor_clean'].value_counts().dropna() #считаем и удаляем с маркером NA
+advisor_counts = data['Advisor_mapped'].value_counts().dropna() #считаем и удаляем с маркером NA
 
 top_10 = advisor_counts.head(10) #берём первые 10 
 
